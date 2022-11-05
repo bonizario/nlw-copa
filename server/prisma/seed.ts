d185111165f7b28c7349169437ec38a4c946cef3
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2022-11-05 19:01:04 -0300

    feat: add users, guesses, and participants models

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      avatarUrl: 'https://github.com/bonizario.png',
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: 'ABC123',
+      ownerId: user.id,
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  let date = new Date();
+  date.setDate(date.getDate() + 1);
+  await prisma.game.create({
+    data: {
+      date: date.toISOString(),
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    },
+  });
+
+  date.setDate(date.getDate() + 1);
+  await prisma.game.create({
+    data: {
+      date: date.toISOString(),
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            } ,
+          },
+        },
+      },
+    },
+  });
+}
+
+main()
