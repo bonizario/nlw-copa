99add610896c23c44055d5d9deb8ad161b51243f
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2022-11-06 20:00:23 -0300

    feat: implement google authentication process

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode } from 'react';
+import { GOOGLE_CLIENT_ID } from '@env';
+import * as AuthSession from 'expo-auth-session';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -11,24 +17,49 @@ interface AuthProviderProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  userIsLoading: boolean;
   signIn: () => Promise<void>;
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, _setUser] = useState<UserProps>({} as UserProps);
+  const [userIsLoading, setUserIsLoading] = useState(false);
+  const [_request, response, promptAsync] = Google.useAuthRequest({
+    clientId: GOOGLE_CLIENT_ID,
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email'],
+  });
+
   async function signIn() {
-    console.log('Login!');
+    try {
+      setUserIsLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setUserIsLoading(false);
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log({ access_token });
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user: {
-          name: 'Gabriel Bonizário',
-          avatarUrl: 'https://github.com/bonizario.png',
-        },
+        userIsLoading,
+        user,
       }}
     >
       {children}
